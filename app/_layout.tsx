8cc44375036fcc1a4465b9d2957cb0fd6b5ac623
Mohamed Isaak <mohamedibrahimisaak10@gmail.com>
2025-02-18 18:43:39 +0300

    create the tab navigation for the project. Do three tabs namely Home, Profile & Explore

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+  <Stack>
+    <Stack.Screen name="(tabs)" options={{
+      headerShown: false,
+      //You can also disable the header on the Stack level above using the same code as above.
+      //You can also remove the headers in the _layout.tsx file inside the Tabs using the 
+      //screenOptions={{ headerShown: false }} prop. This should be on the top-level Tab.
+    }}/>
+  </Stack>
+  );
 }
